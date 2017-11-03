d273fa816faca6cc6b8c3afb13413eb5f33405a9
RajesMca008 <RajesMca008@users.noreply.github.com>
2017-11-03 21:21:03 +0530

    Home screen. Fragments added

--- AdsPro/app/src/main/java/com/appants/adspro/HomeActivity.java
+++ AdsPro/app/src/main/java/com/appants/adspro/HomeActivity.java
@@ -1,48 +1,80 @@
 package com.appants.adspro;
 
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.transition.Transition;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements DashboardFragment.OnFragmentInteractionListener{
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    selectedFragment=DashboardFragment.newInstance("","");
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_mynetwork);
-                    return true;
+                    selectedFragment=new MyNetworkFragment();//MyNetworkFragment.newInstance("","");
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_deals);
-                    return true;
+                    selectedFragment=DealsFragment.newInstance("","");
+                    break;
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
-                    return true;
+                    selectedFragment=SettingsFragment.newInstance("","");
+                    break;
             }
-            return false;
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content, selectedFragment);
+            transaction.commit();
+            return true;
         }
 
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+            //set the transition
+            Transition ts = new Explode();
+            ts.setDuration(5000);
+            getWindow().setEnterTransition(ts);
+            getWindow().setExitTransition(ts);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, DashboardFragment.newInstance("",""));
+        transaction.commit();
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+        Log.i("onFragmentInteraction",":"+uri);
+    }
 }
